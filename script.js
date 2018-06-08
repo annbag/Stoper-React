0d80a699727b3369a1c262d57bd488d2ac87832d
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-08 15:22:29 +0200

    add part task

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Stopwatch extends React.Component {
 				minutes: 0,
 	            seconds: 0,
 	            miliseconds: 0
-			},			
+			},	
 		}
 	}
 	reset() {
@@ -19,10 +19,6 @@ class Stopwatch extends React.Component {
 	        }
     	})
     }
-//czy print się zmienia?
-    print() {
-        this.display.innerText = this.format(this.times);
-	}
 	format(times) {
         return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
 	}
@@ -31,13 +27,12 @@ class Stopwatch extends React.Component {
 	        this.setState({
 	        	running: true,
 		        watch: setInterval(() => this.step(), 10)
-	        })		        
+	        });	        
 	    }
 	}
 	step() {
 	    if (!this.state.running) return;
-	    this.calculate();
-	    this.print();
+	    this.calculate();	
 	}
 	calculate() {
 	    let{minutes, seconds, miliseconds}=this.state.times;
@@ -50,6 +45,14 @@ class Stopwatch extends React.Component {
 	        minutes += 1;
 	        seconds = 0;
 	    }
+	    //aktualizacaj times
+        this.setState({
+	        times: {
+	            minutes,
+	            seconds,
+	            miliseconds
+	        }
+    	})
 	}
 	stop() {
 	    this.setState({
@@ -60,15 +63,39 @@ class Stopwatch extends React.Component {
 	resetwatch() {
 		this.setState({
 			running: false,
+			times: {
+		        minutes: 0,
+		        seconds: 0,
+		        miliseconds: 0
+		    }
 		})
 		clearInterval(this.state.watch);
-		this.reset();
-		this.print();
+		this.reset();		
 	}
+}
+render() {
+	return (
+		<div>
+			<h1>Stoper</h1>
+		    <nav className="controls">
+		      <Button onClick={this.start}>Start</Button>
+		      <Button onClick={this.stop>Stop</Butto>
+		      <Button onClick={this.reset>Reset</Butto>
+		    </nav>
+		    <div class="stopwatch">
+
+		    </div>
+		    <ul class="results">
+
+		    </ul>
+		</div>
+	)
 }
 
 
-const stopwatch = new Stopwatch(
+
+
+/*const stopwatch = new Stopwatch(
 document.querySelector('.stopwatch'));
 
 const startButton = document.getElementById('start');
@@ -78,18 +105,18 @@ const stopButton = document.getElementById('stop');
 stopButton.addEventListener('click', () => stopwatch.stop());
 
 const resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => stopwatch.resetwatch());
+resetButton.addEventListener('click', () => stopwatch.resetwatch());*/
 
 //pad0 ma za zadanie dodać zero do liczb jednocyfrowych
-function pad0(value) {
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
-};
+	function pad0(value) {
+	    let result = value.toString();
+	    if (result.length < 2) {
+	        result = '0' + result;
+	    }
+	    return result;
+	};
 
 ReactDOM.render(
-	<Stopwatch />, 
+	<Stopwatch/>, 
 	document.getElementById('app')
 );
